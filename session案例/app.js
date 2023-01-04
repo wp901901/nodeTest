724377c9e4de26cf80cebb13868da60e57bc15f0
彭于晏禧越分晏 <weipeng@yamyamyam.net>
2023-01-04 18:00:25 +0800

    更新session

---
+++ session案例/app.js
@@ -0,0 +1,63 @@
+// 导入 express 模块
+const express = require('express')
+// 导入express-session模块
+const session = require('express-session');
+// 创建 express 的服务器实例
+const app = express()
+
+
+// TODO_01：请配置 Session 中间件
+app.use(session({
+  secret: 'hello2023', // secret属性的值可以为任意字符串
+  resave: false, // 固定写法
+  saveUninitialized: false,  // 固定写法
+}))
+
+// 托管静态页面
+app.use(express.static('./pages'))
+// 解析 POST 提交过来的表单数据
+app.use(express.urlencoded({ extended: false }))
+
+// 登录的 API 接口
+app.post('/api/login', (req, res) => {
+  // 判断用户提交的登录信息是否正确
+  if (req.body.username !== 'admin' || req.body.password !== '000000') {
+    return res.send({ status: 1, msg: '登录失败' })
+  }
+
+  console.log(res.body);
+  console.log(req.session);
+  // TODO_02：请将登录成功后的用户信息，保存到 Session 中
+  // 注意：只有成功配置了express-session这个中间件后，才能通过req点出来session这个属性
+  req.session.userInfo = res.body;  // 用户登录的信息
+  req.session.isLogin = true; // 用户登录的状态
+  req.send({ status: 0, msg: '登录成功' })
+})
+
+// 获取用户姓名的接口
+app.get('/api/username', (req, res) => {
+  // TODO_03：请从 Session 中获取用户的名称，响应给客户端
+  if (!res.session.isLogin) {
+    return res.send({ status: 1, msg: 'fail' })
+  }
+  res.send({
+    status:0,
+    msg:'success',
+    username:req.session.userInfo.username
+  })
+})
+
+// 退出登录的接口
+app.post('/api/logout', (req, res) => {
+  // TODO_04：清空 Session 信息
+  req.session.destroy();
+  res.send({
+    status:0,
+    msg:'退出登录成功'
+  })
+})
+
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(80, function () {
+  console.log('Express server running at http://127.0.0.1:80')
+})
