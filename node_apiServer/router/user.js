fc17e1859672ae3436a41d21191c43b8240de74e
ICC-EddiePeng <229274940@qq.com>
2023-01-09 21:30:53 +0800

    更新api路由文件，完成数据库引入

--- node_apiServer/router/user.js
+++ node_apiServer/router/user.js
@@ -4,15 +4,15 @@ const express = require('express');
 // 创建路由对象
 const router = express.Router();
 
+// 为了保证路由模块的纯粹性，所有的路由处理函数，必须抽离到对应的路由处理函数模块中
+// 在/router_handler/user.js中，使用exports对象，分别向外共享如下两个处理函数
+const {regUser,login} = require('../router_handler/user.js')
+
 // 注册新用户
-router.post('/reguser',(req,res)=>{
-    res.send('reguser OK')
-})
+router.post('/reguser',regUser)
 
 // 登录
-router.post('/login',(req,res)=>{
-    res.send('login OK')
-})
+router.post('/login',login)
 
 // 将路由对象共享出去
 module.exports = router;
