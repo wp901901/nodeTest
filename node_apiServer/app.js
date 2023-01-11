733feb1d4ec6e6f1e110f0de790f812e3ba5d2e0
ICC-EddiePeng <229274940@qq.com>
2023-01-11 21:29:20 +0800

    使用中间件封装send方法

--- node_apiServer/app.js
+++ node_apiServer/app.js
@@ -13,6 +13,20 @@ app.use(cors())
 // 该配置只能解析application/x-www-form-urlencoded格式的表单数据
 app.use(express.urlencoded({ extended: false }))
 
+// 响应数据的中间件，做一个简单的封装(一定要在路由之前)
+app.use((req,res,next)=>{
+    // status = 0 为成功，status = 1为失败，默认将status的值设置为1，方便处理失败的情况
+    res.cc = function(status = 1,msg){
+        res.send({
+            // 状态
+            status,
+            // 状态描述，判断err是错误对象还是字符串
+            message:msg instanceof Error ? msg.message : msg
+        })
+    }
+    next()
+})
+
 // 导入并注册用户路由模块(并且请求时，需要在拼接/api才能请求，统一的访问前缀)
 const userRouter = require('./router/user')
 app.use('/api',userRouter)
--- node_apiServer/router_handler/user.js
+++ node_apiServer/router_handler/user.js
@@ -28,38 +28,41 @@ exports.regUser = (req, res) => {
     let { username, password } = req.body;
     // 判断数据是否合法
     if (!username || !password) {
-        return res.send({ status: 1, message: '用户名或密码不能为空！' })
+        // return res.send({ status: 1, message: '用户名或密码不能为空！' })
+        return res.cc(1, '用户名或密码不能为空！')
     }
 
     // 执行查询sql语句并判断用户名是否被占用
     db.query(selectSql, [username], (err, result) => {
         // 执行SQL语句失败
-        if(err){
-            return res.send({ status: 1, message: err.message })
+        if (err) {
+            // return res.send({ status: 1, message: err.message })
+            return res.cc(1, err.message)
         }
-        console.log('result',result);
-        if(result.length){
-            return res.send({ status: 1, message: '用户名已被占用' })
+        // console.log('result', result);
+        if (result.length) {
+            // return res.send({ status: 1, message: '用户名已被占用' })
+            return res.cc(1, '用户名已被占用')
         }
         // 对用户的密码进行bcrypt加密，返回值是加密之后的密码字符串
-        password = bcrypt.hashSync(password,10)
-        console.log(password);
+        password = bcrypt.hashSync(password, 10)
+        // console.log(password);
         // 执行插入操作
-        db.query(insertSql,{username,password},(err,result)=>{
+        db.query(insertSql, { username, password }, (err, result) => {
             // 执行SQL语句失败
-            if(err){
-                return res.send({ status: 1, message: err.message })
+            if (err) {
+                return res.cc(1, err.message)
             }
-            if(result.affectedRows !== 1){
-                return res.send({ status: 1, message: '注册用户失败，请稍后再试' })
+            if (result.affectedRows !== 1) {
+                return res.cc(1, '注册用户失败，请稍后再试')
             }
-            return res.send({ status: 0, message: '用户注册成功' })
+            return res.cc(0, '用户注册成功')
         })
-        
 
-        
+
+
     })
-    console.log('username',username);
+    // console.log('username', username);
     // return res.send('reguser OK')
 }
 
