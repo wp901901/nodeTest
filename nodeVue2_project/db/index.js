3e76e003ab24c2ce37647f149d61cde04e6bba7e
ICC-EddiePeng <229274940@qq.com>
2023-04-20 21:11:29 +0800

    新建数据库表

--- nodeVue2_project/db/index.js
+++ nodeVue2_project/db/index.js
@@ -6,8 +6,8 @@ const db = mysql.createPool({
     host:'127.0.0.1',   // 要链接哪个数据库
     user:'',    // 数据库账户名
     password:'',    // 数据库密码
-    database:'',    // 
+    database:'adminsql',    // 通过database属性指定操作哪一个数据库
 })
-
+// https://blog.csdn.net/H1519223775/article/details/127200632
 // 向外共享db数据库连接对象
 module.exports = db;
