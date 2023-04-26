91c28360f0de6d41d0d7b917a944cf8c8d2ecdc2
彭于晏禧越分晏 <weipeng@yamyamyam.net>
2023-04-26 17:59:51 +0800

    更新jwt

--- nodeVue2_project/router_handler/uesr.js
+++ nodeVue2_project/router_handler/uesr.js
@@ -13,7 +13,9 @@ const gravatar = require('gravatar');
  *  优点：加密之后的密码，无法被逆向破解，
  *      同一明文密码多次加密，得到的加密结果各不相同，保证了安全性 
  */
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+// 导入jsonwebtoekn，生成token
+const jwt = require('jsonwebtoken')
 
 // 注册用户的处理函数
 function registerHandler(req, res) {
@@ -44,8 +46,36 @@ function registerHandler(req, res) {
 }
 
 // 用户登录的处理函数
-function loginHandler(req,res) {
-    
+function loginHandler(req, res) {
+    const { email, password } = req.body;
+    const selectSql = `SELECT * FROM ${dbUsers} WHERE email = ?`;
+    db.query(selectSql, { email }, (err, result) => {
+        // 执行SQL语句失败
+        if (err) { return res.cc(err.message, 500) }
+        // 执行SQL语句成功，但是获取到的数据条数不等于1
+        if (result.length != 1) { return res.cc('用户不存在', 404) }
+        // 判断密码是否正确，拿着用户输入的密码，和数据库中存储的密码进行比对
+        /**
+         * 上面注册中使用了bcrypt.hashSync加密用户注册密码，因此在登录时，需要使用bcrypt.compareSync解密密码
+         * 参数1：用户提交的密码
+         * 参数2：数据库中的密码
+         * 返回值是布尔值，true为一致，false问不一致
+         */
+        const compareResult = bcrypt.compareSync(password, result[0].password);
+        if (!compareResult) {
+            return res.cc('用户名或密码错误', 500)
+        }
+        // 登录成功，生成Token字符串（在生成token字符串的时候，不要把密码放进去，这里将头像的值也剔除了）
+        const user = { ...result[0], password: '', avatar: '' };
+        // 登录成功后，调用jwt.sign()方法生成JWT字符串。并通过token属性发送给客户端
+        /**
+         * 参数1：用户的信息对象（可以随意）
+         * 参数2：加密的密钥（可以随意）
+         * 参数3：配置对象，可以配置当前token 的有效期，3600（默认ms）,60s,1h等
+         * 参数4：一个回调函数（这里不用）
+         */
+        const tokenStr = jwt.sign()
+    })
 }
 
 module.exports = {
