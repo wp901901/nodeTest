23793f6afe8ecad75f77700310134477070f9dc3
彭于晏禧越分晏 <weipeng@yamyamyam.net>
2023-05-09 17:59:31 +0800

    新增查询接口

--- nodeVue2_project/router_handler/profiles.js
+++ nodeVue2_project/router_handler/profiles.js
@@ -5,15 +5,15 @@
 // 引入数据库操作模块
 const db = require('../db/mysqlDB')
 // 用户数据库名字
-const dbUsers = 'profile';
+const dbProfile = 'profile';
 
 // 添加的处理函数
 function addHandler(req, res) {
     // 定义插入语句
-    const insertSql = `INSERT INTO ${dbUsers} SET ?`;
+    const insertSql = `INSERT INTO ${dbProfile} SET ?`;
     const { type, describe, income, expend, cash, remark } = req.body;
     if (!req.body.type) { return res.cc('type值为空') }
-    db.query(insertSql, {type, describe, income, expend, cash, remark}, (err, result) => {
+    db.query(insertSql, { type, describe, income, expend, cash, remark }, (err, result) => {
         // 执行SQL语句失败
         if (err) {
             // return res.send({ status: 1, message: err.message })
@@ -26,9 +26,42 @@ function addHandler(req, res) {
     })
 }
 
+// 查询所有数据的处理函数
+function selectAllDataHandler(req, res) {
+    const selSql = `SELECT * FROM ${dbProfile}`;
+    console.log(321);
+    db.query(selSql, (err, result) => {
+        // 执行SQL语句失败
+        if (err) {
+            // return res.send({ status: 1, message: err.message })
+            return res.cc(err.message)
+        }
+        res.cc(result, 200)
+    })
+}
+
+// 查询单个数据的处理函数
+function selectDataHandle(req, res) {
+    const { id } = req.body;
+    const selSql = `SELECT * FROM ${dbProfile} WHERE id = ?`;
+    db.query(selSql, [id], (err, result) => {
+        console.log(result);
+        // 执行SQL语句失败
+        if (err) {
+            return res.cc(err.message)
+        }
+        if (result.length !== 1) {
+            return res.cc('查询错误，请稍后再试！')
+        }
+        res.cc(result, 200)
+    })
+}
+
 
 
 
 module.exports = {
-    addHandler
+    addHandler,
+    selectAllDataHandler,
+    selectDataHandle
 }
--- nodeVue2_project/routers/api/profiles.js
+++ nodeVue2_project/routers/api/profiles.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 // 为了保证路由模块的纯粹性，所有的路由处理函数，必须抽离到对应的路由处理函数模块中
 // 在/router_handler/profiles.js中，使用exports对象，分向外共享处理函数
-const { addHandler } = require('../../router_handler/profiles');
+const { addHandler, selectAllDataHandler, selectDataHandle } = require('../../router_handler/profiles');
 
 
 // 添加接口
 router.post("/add", addHandler)
+// 查询所有数据接口
+router.post("/selectAllData", selectAllDataHandler)
+// 查询单个数据接口
+router.post("/selectData", selectDataHandle)
 
 
 module.exports = router;
